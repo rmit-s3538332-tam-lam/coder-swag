f9f0e4388a67da402fd6bf4e38a70464b37a168c
Graphene <graphene@Graphene.local>
2019-01-25 14:05:03 +0700

    collection view for each category

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Graphene on 25/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage : UIImageView!
+    @IBOutlet weak var productTitle : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
+    
+}
